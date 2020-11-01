f1a5f347560dd12552e1a54353d9f84a28ab6b6c
Enygeek <61242498+Enygeek@users.noreply.github.com>
2020-11-01 02:15:40 +0100

    09

---
+++ admin/versement/traitement/nav.php
@@ -0,0 +1,98 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <title>JI_MIAGE</title>
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+  <meta content="" name="keywords">
+  <meta content="" name="description">
+
+  <!-- Favicons -->
+  <!-- Google Fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,700,700i|Montserrat:300,400,500,700" rel="stylesheet">
+
+  <!-- Bootstrap CSS File -->
+  <link href="../lib/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+  <!-- Libraries CSS Files -->
+  <link href="../..lib/font-awesome/css/font-awesome.min.css" rel="stylesheet">
+  <link href="../../lib/animate/animate.min.css" rel="stylesheet">
+  <link href="../../lib/ionicons/css/ionicons.min.css" rel="stylesheet">
+  <link href="../../lib/owlcarousel/assets/owl.carousel.min.css" rel="stylesheet">
+  <link href="../../lib/lightbox/css/lightbox.min.css" rel="stylesheet">
+
+  <!-- Main Stylesheet File -->
+  <link rel="stylesheet" href="../../css/style_prog.css">
+  <link href="../../css/style.css" rel="stylesheet">
+  
+  <!-- JQuery importer by le_Z -->
+  <script src="../../lib/jquery/jquery.min.js"></script>
+  <!-- =======================================================
+    Theme Name: BizPage
+    Theme URL: https://bootstrapmade.com/bizpage-bootstrap-business-template/
+    Author: BootstrapMade.com
+    License: https://bootstrapmade.com/license/
+  ======================================================= -->
+
+  <style type="text/css">
+    header{
+      background-color: rgba(0,0,0,0.3);
+    }
+  </style>
+  
+
+</head>
+
+
+
+<body>
+
+  <!--==========================
+    Header
+  ============================-->
+  <header id="header">
+    <div class="container-fluid">
+
+      <div id="logo" class="pull-left">
+        <h1><a href="../../index.php" style="margin-left:50%" class="scrollto">JI_MIAGE</a></h1>
+        <!-- Uncomment below if you prefer to use an image logo -->
+        <a href="../../index.php"><img id="tl" src="../../img/logo.png" alt="" title="" /></a>
+      </div>
+        
+
+      <nav id="nav-menu-container">
+        <ul class="nav-menu">
+          <li class="menu-active"><a href="../../index.php"><h1 id="ti">JI MIAGE</h1></a></li>
+          <li><a href="../../index.php">Accueil</a></li>
+          <!-- <li><a href="#skills">SE CONNECTER</a></li> -->
+          <!-- <li><a href="inscription/">S'INSCRIRE</a></li> -->
+          <!-- <li><a href="#contact">CONTACTEZ-NOUS</a></li> -->
+          <!-- <li class="menu-has-children"><a href="">Plus+</a>
+            <ul>
+              <li><a href="#">En savoir plus</a></li>
+              <li><a href="#">Mot du PCO</a></li>
+              <li><a href="#">Speakers/intervenant</a></li>
+              <li><a href="#">Programme</a></li>
+              <li><a href="#">Lieu</a></li>
+              <li><a href="#">Contactez-nous</a></li>
+            </ul>
+          </li> -->
+        </ul>
+      </nav><!-- #nav-menu-container -->
+    </div>
+  </header><!-- #header -->
+
+
+  <!-- Script by KeraCode Inc. -->
+  <script>
+    $(function(){
+      $("#ti").hide();
+      // $("#p1").click(function(){
+          
+      // });
+    });
+
+  </script>
+
+  
+  <!-- JavaScript Libraries -->
