e0dcc777aca3eb8bde7fb99b3c3aae14db1cc5e8
Enygeek <61242498+Enygeek@users.noreply.github.com>
2020-11-01 02:15:19 +0100

    ji06

---
+++ admin/partenaire.php
@@ -0,0 +1,30 @@
+<!--==========================
+      Clients Section
+    ============================-->
+    <section id="clients" class="wow fadeInUp mb-3 mt-3">
+      <div class="container">
+
+        <header class="section-header">
+          <h3>Nos partenaires </h3>
+          <!-- <i><h6>Ils nous font confiances</h6></i> -->
+        </header>
+
+        <div class="owl-carousel clients-carousel">
+          <img src="img/clients/client-1.png" alt="">
+          <img src="img/clients/client-2.png" alt="">
+          <img src="img/clients/client-3.png" alt="">
+          <img src="img/clients/client-4.png" alt="">
+          <img src="img/clients/client-5.png" alt="">
+          <img src="img/clients/client-6.png" alt="">
+          <img src="img/clients/client-7.png" alt="">
+          <img src="img/clients/client-8.png" alt="">
+        </div>
+
+      </div>
+    </section><!-- #clients -->
+
+
+    <!-- 
+      Pour les logo des partenaires :
+      _Taille img : 185 * 75
+      _Ajouter les nom comme sous titre de chaque logo -->
